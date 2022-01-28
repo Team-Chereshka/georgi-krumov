6c68f1ec37643841dbfe3a7aa601d1f6621d6e6c
George Krumov <61293783+georgik1@users.noreply.github.com>
2022-01-28 14:56:58 +0200

    Delete src directory

--- src/CherryBerry.java
+++
@@ -1,9 +0,0 @@
-public class CherryBerry extends Vegetable {
-
-    private static final int STAMINA = 10;
-
-    public CherryBerry(String name, int row, int col, int movesToGrow) {
-        super(name, row, col, movesToGrow);
-        this.setStamina(STAMINA);
-    }
-}
--- src/MeloLemonMelon.java
+++
@@ -1,8 +0,0 @@
-public class MeloLemonMelon extends Vegetable{
-
-
-    public MeloLemonMelon(String name, int row, int col, int movesToGrow) {
-        super(name, row, col, movesToGrow);
-    }
-
-}
