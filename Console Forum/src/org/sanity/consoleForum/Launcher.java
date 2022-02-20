ee25e5fc6870a2eb28aa7fe206099bae7eabf913
George Krumov <61293783+georgik1@users.noreply.github.com>
2022-02-20 12:38:48 +0200

    Update Launcher class

---
+++ Console Forum/src/org/sanity/consoleForum/Launcher.java
@@ -0,0 +1,46 @@
+package org.sanity.consoleForum;
+
+import org.sanity.consoleForum.commands.AboutCommand;
+import org.sanity.consoleForum.commands.AllPostsCommand;
+import org.sanity.consoleForum.commands.ClearCommand;
+import org.sanity.consoleForum.commands.CommentCommand;
+import org.sanity.consoleForum.commands.CreatePostCommand;
+import org.sanity.consoleForum.commands.DeleteCommentCommand;
+import org.sanity.consoleForum.commands.DeletePostCommand;
+import org.sanity.consoleForum.commands.DemoteCommand;
+import org.sanity.consoleForum.commands.DislikeCommand;
+import org.sanity.consoleForum.commands.EditCommentCommand;
+import org.sanity.consoleForum.commands.EditPostCommand;
+import org.sanity.consoleForum.commands.ExitCommand;
+import org.sanity.consoleForum.commands.HelpCommand;
+import org.sanity.consoleForum.commands.LikeCommand;
+import org.sanity.consoleForum.commands.LoginCommand;
+import org.sanity.consoleForum.commands.LogoutCommand;
+import org.sanity.consoleForum.commands.PromoteCommand;
+import org.sanity.consoleForum.commands.ReadCommand;
+import org.sanity.consoleForum.commands.RegisterCommand;
+import org.sanity.consoleForum.core.CommandManager;
+import org.sanity.consoleForum.core.ConsoleForumCommandManager;
+import org.sanity.consoleForum.core.ConsoleForumEngine;
+import org.sanity.consoleForum.database.EntityManager;
+import org.sanity.consoleForum.io.ConsoleReader;
+import org.sanity.consoleForum.io.ConsoleWriter;
+import org.sanity.consoleForum.io.InputReader;
+import org.sanity.consoleForum.io.OutputWriter;
+
+public class Launcher {
+    public static void main(String[] args) {
+
+        InputReader consoleReader = new ConsoleReader();
+        OutputWriter consoleWriter = new ConsoleWriter();
+        EntityManager entityManager = new EntityManager();
+
+        CommandManager commandManager = new ConsoleForumCommandManager(consoleReader, consoleWriter, entityManager );
+
+
+        ConsoleForumEngine consoleForumEngine = new ConsoleForumEngine(commandManager, entityManager);
+
+
+       consoleForumEngine.run();
+    }
+}
